6b7aa057c1ac4b391ce43ee09cd36241702313c2
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-04-05 11:33:39 +0700

    menambahkan class SayaMusicTracks

--- TP_MODUL6_103022400095/Program.cs
+++ TP_MODUL6_103022400095/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("TUGAS PENDAHULUAN MODUL 6 KONSTRUKSI PERANGKAT LUNAK");
+﻿using TP_MODUL6_103022400095;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("TUGAS PENDAHULUAN MODUL 6 KONSTRUKSI PERANGKAT LUNAK");
+
+        SayaMusikTracks track1 = new SayaMusikTracks("Bermekaran dan Mewangi - Pendarra");
+        track1.IncreasePlayCount(100);
+        track1.PrintTrackDetails();
+
+    }
+}
---
+++ TP_MODUL6_103022400095/SayaMusikTracks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400095
+{
+    internal class SayaMusikTracks
+    {
+        private int id;
+        private string playCount;
+        private string title;
+
+        public SayaMusikTracks(string title)
+        {
+            this.title = title;
+            this.playCount = "0";
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount = (int.Parse(playCount) + count).ToString();
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
